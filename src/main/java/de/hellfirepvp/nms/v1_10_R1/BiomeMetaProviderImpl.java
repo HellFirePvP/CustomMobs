a605553391daffdd5dd13f5b2a003b348c846f3e
HellFirePvP <dendorfer.maximilian0@googlemail.com>
2016-06-23 16:41:35 +0200

    - Compatibility for mc 1.10

---
+++ src/main/java/de/hellfirepvp/nms/v1_10_R1/BiomeMetaProviderImpl.java
@@ -0,0 +1,131 @@
+package de.hellfirepvp.nms.v1_10_R1;
+
+import de.hellfirepvp.nms.BiomeMetaProvider;
+import de.hellfirepvp.nms.NMSReflector;
+import net.minecraft.server.v1_10_R1.BiomeBase;
+import net.minecraft.server.v1_10_R1.EntityInsentient;
+import net.minecraft.server.v1_10_R1.EntityTypes;
+import net.minecraft.server.v1_10_R1.EnumCreatureType;
+import net.minecraft.server.v1_10_R1.WeightedRandom;
+
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * This class is part of the CustomMobs Plugin
+ * The plugin can be found at: https://www.spigotmc.org/resources/custommobs.7339
+ * Class: BiomeMetaProviderImpl
+ * Created by HellFirePvP
+ * Date: 23.06.2016 / 16:23
+ */
+public class BiomeMetaProviderImpl implements BiomeMetaProvider {
+
+    @Override
+    public List getBiomes() {
+        return new LinkedList<>(BiomeBase.i);
+    }
+
+    @Override
+    public Map<String, Map<String, List<BiomeMetaProvider.NMSBiomeMetaLink>>> getSortedBiomeMeta() {
+        Map<String, Map<String, List<BiomeMetaProvider.NMSBiomeMetaLink>>> dataOutput = new HashMap<>();
+        for(BiomeBase base : new LinkedList<>(BiomeBase.i)) {
+            if(base == null) continue;
+
+            String name = base.l();
+            Map<String, List<BiomeMetaProvider.NMSBiomeMetaLink>> specData = new HashMap<>();
+            for(EnumCreatureType type : EnumCreatureType.values()) {
+                if (type == null) continue;
+
+                List biomeMetaList = base.getMobs(type);
+                specData.put(type.name(), getNMSRepresentation(biomeMetaList));
+            }
+            dataOutput.put(name, specData);
+        }
+        return dataOutput;
+    }
+
+    private List<BiomeMetaProvider.NMSBiomeMetaLink> getNMSRepresentation(List metas) {
+        List<BiomeMetaProvider.NMSBiomeMetaLink> biomeMetaLinks = new LinkedList<>();
+        for(Object objMeta : metas) {
+            if(objMeta == null || !(objMeta instanceof BiomeBase.BiomeMeta)) continue;
+
+            BiomeBase.BiomeMeta meta = (BiomeBase.BiomeMeta) objMeta;
+            int weightedChance = NMSReflector.getField("a", WeightedRandom.WeightedRandomChoice.class, meta, int.class);
+            String typeStr = getEntityTypeString(meta.b);
+            if(typeStr == null) continue;
+            biomeMetaLinks.add(new BiomeMetaProvider.NMSBiomeMetaLink(weightedChance, meta.c, meta.d, typeStr));
+        }
+        return biomeMetaLinks;
+    }
+
+    private String getEntityTypeString(Class<?> entityClass) {
+        Map classToString = NMSReflector.getField("d", EntityTypes.class, null, Map.class);
+        return (String) classToString.get(entityClass);
+    }
+
+    private Class<? extends EntityInsentient> getEntityTypeClass(String name) {
+        Map stringToClass = NMSReflector.getField("c", EntityTypes.class, null, Map.class);
+        try {
+            return (Class<? extends EntityInsentient>) stringToClass.get(name);
+        } catch (Throwable tr) {
+            return null;
+        }
+    }
+
+    @Override
+    public boolean applyBiomeData(Map<String, Map<String, List<BiomeMetaProvider.NMSBiomeMetaLink>>> data) {
+        try {
+            for(String biomeName : data.keySet()) {
+                if(biomeName == null) continue;
+                BiomeBase base = getBiomeBaseByName(biomeName);
+                if(base == null) continue;
+
+                Map<String, List<BiomeMetaProvider.NMSBiomeMetaLink>> specData = data.get(biomeName);
+                for(String target : specData.keySet()) {
+                    if(target == null) continue;
+                    EnumCreatureType type;
+                    try {
+                        type = EnumCreatureType.valueOf(target);
+                    } catch (Throwable tr) {
+                        continue;
+                    }
+
+                    if(type == null) continue;
+
+                    List<BiomeBase.BiomeMeta> metas = getMetaList(specData.get(target));
+
+                    List originalBiomeMetas = base.getMobs(type);
+                    originalBiomeMetas.clear();
+                    originalBiomeMetas.addAll(metas);
+                }
+            }
+
+            return true;
+        } catch (Throwable tr) {
+            return false;
+        }
+    }
+
+    private List<BiomeBase.BiomeMeta> getMetaList(List<BiomeMetaProvider.NMSBiomeMetaLink> links) {
+        List<BiomeBase.BiomeMeta> metas = new LinkedList<>();
+        for(BiomeMetaProvider.NMSBiomeMetaLink link : links) {
+            if(link == null) continue;
+
+            Class<? extends EntityInsentient> entityClass = getEntityTypeClass(link.entityClassStr);
+
+            if(entityClass == null) continue;
+            metas.add(new BiomeBase.BiomeMeta(entityClass, link.weightedMobChance, link.minimumCount, link.maximumCount));
+        }
+        return metas;
+    }
+
+    private BiomeBase getBiomeBaseByName(String name) {
+        for(BiomeBase base : new LinkedList<>(BiomeBase.i)) {
+            if(base == null) continue;
+            if(base.l().equalsIgnoreCase(name)) return base;
+        }
+        return null;
+    }
+}
---
+++ src/main/java/de/hellfirepvp/nms/v1_10_R1/NMSUtilImpl.java
@@ -0,0 +1,145 @@
+package de.hellfirepvp.nms.v1_10_R1;
+
+import de.hellfirepvp.CustomMobs;
+import de.hellfirepvp.nms.NMSUtils;
+import de.hellfirepvp.spawning.worldSpawning.WorldSpawner;
+import net.minecraft.server.v1_10_R1.BlockPosition;
+import net.minecraft.server.v1_10_R1.Blocks;
+import net.minecraft.server.v1_10_R1.EntityLiving;
+import net.minecraft.server.v1_10_R1.EnumCreatureType;
+import net.minecraft.server.v1_10_R1.EnumItemSlot;
+import net.minecraft.server.v1_10_R1.IBlockData;
+import net.minecraft.server.v1_10_R1.Item;
+import net.minecraft.server.v1_10_R1.ItemStack;
+import net.minecraft.server.v1_10_R1.WorldServer;
+import org.bukkit.Location;
+import org.bukkit.World;
+import org.bukkit.block.Block;
+import org.bukkit.block.CreatureSpawner;
+import org.bukkit.craftbukkit.v1_10_R1.CraftWorld;
+import org.bukkit.craftbukkit.v1_10_R1.block.CraftCreatureSpawner;
+import org.bukkit.craftbukkit.v1_10_R1.entity.CraftLivingEntity;
+import org.bukkit.entity.LivingEntity;
+import org.bukkit.util.Vector;
+
+import java.lang.reflect.Field;
+
+/**
+ * This class is part of the CustomMobs Plugin
+ * The plugin can be found at: https://www.spigotmc.org/resources/custommobs.7339
+ * Class: NMSUtilImpl
+ * Created by HellFirePvP
+ * Date: 23.06.2016 / 16:30
+ */
+public class NMSUtilImpl implements NMSUtils {
+
+    @Override
+    public void setName(LivingEntity entity, String name) {
+        EntityLiving living = ((CraftLivingEntity) entity).getHandle();
+        try {
+            ItemStack stack = living.getEquipment(EnumItemSlot.LEGS);
+            if(stack == null) {
+                stack = new ItemStack(Item.getItemOf(Blocks.STONE));
+            }
+            stack.c(name);
+            living.setSlot(EnumItemSlot.LEGS, stack);
+        } catch (Exception exc) {
+            CustomMobs.logger.severe("Saving name in NBT failed. Cause: " + exc.getMessage());
+        }
+    }
+
+    @Override
+    public String getName(LivingEntity entity) {
+        EntityLiving living = ((CraftLivingEntity) entity).getHandle();
+        try {
+            ItemStack stack = living.getEquipment(EnumItemSlot.LEGS);
+            if(stack == null) return null;
+            if(!stack.hasName()) return null;
+            return stack.getName();
+        } catch (Exception ignored) {}
+        return null;
+    }
+
+    private static Field spigotCfgField;
+    private static boolean isSpigot = true;
+
+    @Override
+    public byte[] getMobSpawnRangeAndViewDistance(World world) {
+        net.minecraft.server.v1_10_R1.WorldServer ws = ((CraftWorld) world).getHandle();
+        if(spigotCfgField == null && isSpigot) {
+            try {
+                spigotCfgField = net.minecraft.server.v1_10_R1.World.class.getDeclaredField("spigotConfig");
+            } catch (NoSuchFieldException e) {
+                isSpigot = false;
+                return getMobSpawnRangeAndViewDistance(world);
+            }
+        }
+        byte[] data = new byte[2];
+        byte viewDistance, mobSpawnRange;
+        if(isSpigot && spigotCfgField != null) {
+            spigotCfgField.setAccessible(true);
+            try {
+                Object spigotCfg = spigotCfgField.get(ws);
+                int viewDst = (int) spigotCfg.getClass().getDeclaredField("viewDistance").get(spigotCfg);
+                viewDistance = (byte) viewDst;
+                mobSpawnRange = (byte) spigotCfg.getClass().getDeclaredField("mobSpawnRange").get(spigotCfg);
+            } catch (Exception e) {
+                isSpigot = false;
+                return getMobSpawnRangeAndViewDistance(world);
+            }
+        } else {
+            viewDistance = 8;
+            mobSpawnRange = 8;
+        }
+        data[0] = mobSpawnRange;
+        data[1] = viewDistance;
+        return data;
+    }
+
+    @Override
+    public int getLivingCount(WorldSpawner.CreatureType creatureType, World world) {
+        EnumCreatureType type;
+        switch (creatureType) {
+            case AMBIENT:
+                type = EnumCreatureType.AMBIENT;
+                break;
+            case WATER_CREATURE:
+                type = EnumCreatureType.WATER_CREATURE;
+                break;
+            case ANIMAL:
+                type = EnumCreatureType.CREATURE;
+                break;
+            case MONSTER:
+                type = EnumCreatureType.MONSTER;
+                break;
+            default:
+                return 0;
+        }
+        WorldServer ws = ((CraftWorld) world).getHandle();
+        return ws.a(type.a());
+    }
+
+    @Override
+    public boolean isPlayerInRange(Location location, double range) {
+        return ((CraftWorld) location.getWorld()).getHandle().a(location.getX(), location.getY(), location.getZ(), range, false) != null;
+    }
+
+    @Override
+    public void clearSpawner(Block block) {
+        org.bukkit.block.BlockState state = block.getState();
+        if(state == null || !(block instanceof CreatureSpawner)) {
+            return;
+        }
+        try {
+            CraftCreatureSpawner spawner = (CraftCreatureSpawner) state;
+            spawner.getTileEntity().getSpawner().setMobName("");
+        } catch (Exception ignored) {}
+    }
+
+    @Override
+    public boolean isNormalCube(World world, Vector at) {
+        WorldServer ws = ((CraftWorld) world).getHandle();
+        IBlockData state = ws.getType(new BlockPosition(at.getBlockX(), at.getBlockY(), at.getBlockZ()));
+        return state.l();
+    }
+}
